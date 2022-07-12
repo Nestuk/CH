bb5725b3c31e18a5401b2679071345512aee0f0c
Nestuk <106037105+Nestuk@users.noreply.github.com>
2022-07-12 18:26:19 -0300

    Update NestorAlvarez.js

--- js/NestorAlvarez.js
+++ js/NestorAlvarez.js
@@ -1,4 +1,4 @@
-fetch('https://62cdde90066bd2b6992d6de1.mockapi.io/JASON/SPLATT')
+fetch('https://mockapi.io/clone/62cdde90066bd2b6992d6de2')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.log(error));
